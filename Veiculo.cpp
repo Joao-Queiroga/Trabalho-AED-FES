dcc998b7f68f8e96a3cbc2e835b1f34735d9aa57
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2022-07-03 21:27:31 -0300

    classe atualizada

--- Veiculo.cpp
+++ Veiculo.cpp
@@ -13,31 +13,47 @@ class Veiculo {
     char status;
 
     public:
+    int getCodigo() { return this->codigo; }
+
     void setDescricao(string descricao) {
         this->descricao = descricao;
     }
+
+    string getDescricao() { return this->descricao; }
 
     void setModelo(string modelo) {
         this->modelo = modelo;
     }
 
+    string getModelo() { return this->modelo; }
+
     void setCor(string cor) {
         this->cor = cor;
     }
+
+    string getCor() { return this->cor; }
 
     void setPlaca(string placa) {
         this->placa = placa;
     }
 
+    string getPlaca() { return this->placa; }
+
     void setDiaria(float diaria) {
         this->diaria = diaria;
     }
+
+    float getDiaria() { return this->diaria; }
 
     void setOcupantes(int ocupantes) {
         this->ocupantes = ocupantes;
     }
 
+    int getOcupantes() { return this->ocupantes; }
+
     void setStatus(char status) {
         this->status = status;
     }
+
+    char getStatus() { return this->status; }
 };
